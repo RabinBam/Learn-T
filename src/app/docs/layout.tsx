29507893d80496271cb9f587d115909c8fdeb444
RabinBam <aaditbam9868@gmail.com>
2025-08-27 00:03:09 +0545

    Changed the preheading inside the docs file system

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 
-import { FooterMeta, FooterSitemap } from "@/components/footer";
-import { Header } from "@/components/header";
-import { MobileNav } from "@/components/mobile-docs-nav";
 import { DocsSidebar } from "@/components/docs-sidebar";
 import { DocsSidebarAutoscroll } from "@/components/docs-sidebar-autoscroll";
 import TableOfContents from "@/components/table-of-contents";
@@ -10,23 +7,14 @@ import { RandomPromo } from "@/components/promos";
 
 export default async function Layout({
   children,
-  breadcrumb,
-}: React.PropsWithChildren<{ breadcrumb: React.ReactNode }>) {
+}: React.PropsWithChildren<{ breadcrumb?: React.ReactNode }>) {
   return (
     <div className="relative min-h-screen bg-gradient-to-br from-purple-900 via-blue-900 to-indigo-900 text-gray-100">
-      {/* Header */}
-      <div className="fixed inset-x-0 top-0 z-20 border-b border-white/10 bg-black/40 backdrop-blur">
-        <Header />
-        <MobileNav header={<Header />} breadcrumb={breadcrumb}>
-          <DocsSidebar />
-        </MobileNav>
-      </div>
-
       {/* Grid Layout */}
-      <div className="grid min-h-screen grid-cols-1 grid-rows-[1fr_auto_auto] pt-20 lg:grid-cols-[var(--container-2xs)_2.5rem_minmax(0,1fr)_2.5rem_var(--container-2xs)] lg:pt-16 xl:grid-cols-[var(--container-2xs)_2.5rem_minmax(0,1fr)_2.5rem_var(--container-2xs)]">
+      <div className="grid min-h-screen grid-cols-1 grid-rows-[1fr_auto_auto] lg:grid-cols-[var(--container-2xs)_2.5rem_minmax(0,1fr)_2.5rem_var(--container-2xs)]">
         {/* LEFT SIDEBAR */}
         <div className="relative col-start-1 row-span-full row-start-1 max-lg:hidden">
-          <div className="sticky top-16 bottom-0 left-0 h-[calc(100vh-4rem)] overflow-y-auto p-6">
+          <div className="sticky top-20 bottom-0 left-0 h-[calc(100vh-5rem)] overflow-y-auto p-6">
             <DocsSidebarAutoscroll>
               <DocsSidebar />
             </DocsSidebarAutoscroll>
@@ -43,10 +31,8 @@ export default async function Layout({
 
         {/* RIGHT SIDEBAR */}
         <div className="relative col-start-5 row-span-full row-start-1 max-lg:hidden">
-          <div className="sticky top-16 bottom-0 right-0 h-[calc(100vh-4rem)] overflow-y-auto p-6 space-y-8">
-            {/* Do NOT let the TOC show a fallback promo; it should render nothing when empty */}
+          <div className="sticky top-20 bottom-0 right-0 h-[calc(100vh-5rem)] overflow-y-auto p-6 space-y-8">
             <TableOfContents tableOfContents={[]} />
-            {/* Promo appears only here */}
             <RandomPromo />
           </div>
         </div>
