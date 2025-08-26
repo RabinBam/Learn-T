4f56b9aafeb03baa8fb7764050ade48f09e25130
RabinBam <aaditbam9868@gmail.com>
2025-08-26 20:11:46 +0545

    Added the changed files

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
+
+// Enable MDX support
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Make Next.js recognize .mdx files as pages/components
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
