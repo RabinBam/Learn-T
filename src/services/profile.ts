bf0bfc66dfc56e647992de86aaa83b69a5a428da
RabinBam <aaditbam9868@gmail.com>
2025-08-27 13:20:05 +0545

    Few tweaks for the firebase

--- src/services/profile.ts
+++ src/services/profile.ts
@@ -1,6 +1,6 @@
 // Firestore-backed user profile helpers.
 
-import { db, storage } from "@/lib/firebase/firebase";
+import { db, storage } from "@/lib/firebase";
 import {
   doc, getDoc, setDoc, updateDoc, serverTimestamp,
 } from "firebase/firestore";
